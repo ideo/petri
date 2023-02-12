30a0dd3e92bdfa358da368cde89a7e0ef69efde9
JessicaFreaner <JessicaFreaner>
2023-02-12 18:47:08 +0000

    move logic into functions

--- main.py
+++ main.py
@@ -2,43 +2,59 @@ import streamlit as st
 import pandas as pd
 
 
-# load baseline excel data
-df = pd.read_excel("D FORD ACCESS (1).xlsx")
+def fix_headers(df):
+    # fix headers - weird split across 2 rows
+    return df.rename(
+        columns={'Person': 'Person Type', 'Access': 'Access Date', },
+    )
 
 
-# fix headers - weird split across 2 rows
-df.rename(
-    columns={'Person': 'Person Type', 'Access': 'Access Date', },
-    inplace=True,
-)
+def remove_junk(df):
+    # gets rid of empty rows & that weird split if it's there in future
+    df.drop(columns=['Category Used'], inplace=True)  # empty column
+    return df.dropna()
 
 
-# get rid of junk
-df.drop(columns=['Category Used'], inplace=True)  # empty column
-df.dropna(inplace=True)  # gets rid of empty rows & that weird split if it's there in future
+def fix_dates(df):
+    # convert to string to date
+    df['Access Date'] = pd.to_datetime(df['Access Date'], format='%d/%m/%Y')
+    df['Access Date'] = df['Access Date'].dt.date
+    return df
 
 
-# convert to string to date
-df['Access Date'] = pd.to_datetime(df['Access Date'], format='%d/%m/%Y')
-df['Access Date'] = df['Access Date'].dt.date
+def anonymize(df):
+    # if we want to make this more sophisticated... look at hashlib
+    df['anon_id'] = df['CDSID'].astype('category').cat.codes
+    return df.drop(columns=['Last Name', 'First Name', 'CDSID'])
 
 
-# anonymize
-# if we want to make this more sophisticated... look at hashlib
-df['anon_id'] = df['CDSID'].astype('category').cat.codes
-df.drop(columns=['Last Name', 'First Name', 'CDSID'], inplace=True)
+def count_one_swipe_per_day(df):
+    # we only care if person's card was used during a given day
+    df.drop(columns=['Person Type', 'Reader Description', 'Transaction Type'], inplace=True)
+    return df.drop_duplicates(ignore_index=True)
 
 
-# we only care if person's card was used during a given day
-df.drop(columns=['Person Type', 'Reader Description', 'Transaction Type'], inplace=True)
-df.drop_duplicates(inplace=True, ignore_index=True)
+def clean_df(df):
+    df = fix_headers(df)
+    df = remove_junk(df)
+    df = fix_dates(df)
+    df = anonymize(df)
+    df = count_one_swipe_per_day(df)
+    return df
 
 
-# App Output
-st.title("Baseline Door Data!")  # add a title
-st.write(df.head())
+def load_baseline(f="D FORD ACCESS (1).xlsx"):
+    df = pd.read_excel(f)
+    df = clean_df(df)
+    return df
 
 
 # st.code(df.dtypes)
 if __name__ == "__main__":
     print("Hello, World!")
+
+    df = load_baseline()
+
+    # App Output
+    st.title("Baseline Door Data!")  # add a title
+    st.write(df.head())
